46c2a455330030a734f6c6495f3a5165e3ba402b
Ajay Choudhary <choudharyajay1323@gmail.com>
2026-07-17 14:21:43 +0530

    port changed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import menuRouter from "./Routes/menuRoute.js"
 import connectCloudinary from "./config/cloudinary.js"
 
 
-const port=4000
+const port=process.env.PORT || 4000
 const app=express()
 
 app.use(cors())
